9ddf06fe12164bdf26853d99c2362bda419e0fb0
Unknown <sergiosanpol@gmail.com>
2018-05-06 12:42:10 +0200

    Auto stash before revert of "Merge remote-tracking branch 'origin/Developer' into Sergio"

--- Scripts/Ventana.gd
+++ Scripts/Ventana.gd
@@ -7,6 +7,11 @@ extends Sprite
 onready var node = get_node("TextureRect")
 onready var tiempo = get_node("Timer")
 onready var texto = get_node("RichTextLabel")
+var texto_face = ["Hoy es el cumpleaños de Pepi", "Gregorio ha compartido tu publicación.", "Te han etiquetado en 616 fotos nuevas."]
+var texto_insta = ["@xinooooo ha subido una foto.", "@Goku86586fase te ha mencionado.", "@lubl te ha solicitado amistad."]
+var texto_twitter = ["#JamtodayAl3", "Traf_de_org ha comenzado a seguirte.", "Mensaje directo de @Thanos."]
+var texto_what = ["Me has dejado en vista??", "Aa mama te ha enviado un mensaje.", "Reenvía esta cadena para..."]
+var texto_you = ["gurifrisky97 chupito lejía challenge.", "Colorín1/4 video reacción video reacción mirando a la pared.", "eR_muelas31415926 gameplay chair simulator 2016"]
 var player 
 var evento_tarjetas
 var num
@@ -15,23 +20,28 @@ func _ready():
 	tiempo.start()
 	randomize()
 	num = int(rand_range(1,6))
+	var text_num = int(rand_range(0,3)) 
 	var imagen
-	texto.set_text("Hola")
 	 	
 	if num == 1:
 		imagen = load("res://Imagenes/face.png")
+		texto.set_text(texto_face[text_num])
 		node.set_texture(imagen)
 	elif num == 2:
 		imagen = load("res://Imagenes/insta.png")
+		texto.set_text(texto_insta[text_num])
 		node.set_texture(imagen)
 	elif num == 3:
 		imagen = load("res://Imagenes/twi.png")
+		texto.set_text(texto_twitter[text_num])
 		node.set_texture(imagen)
 	elif num == 4:
 		imagen = load("res://Imagenes/wapp.png")
+		texto.set_text(texto_what[text_num])
 		node.set_texture(imagen)
 	elif num == 5:
 		imagen = load("res://Imagenes/you.png")
+		texto.set_text(texto_you[text_num])
 		node.set_texture(imagen)
 		
 	set_process(true)
